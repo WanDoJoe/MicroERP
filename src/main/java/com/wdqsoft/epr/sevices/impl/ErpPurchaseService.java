a87dd70e19a302645822b7454ed9d0f29ef5c5ce
WanDoJoe <wandongqiao1990@gmail.com>
2020-08-10 17:34:39 +0800

    增加表

---
+++ src/main/java/com/wdqsoft/epr/sevices/impl/ErpPurchaseService.java
@@ -0,0 +1,12 @@
+package com.wdqsoft.epr.sevices.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import com.wdqsoft.epr.sevices.ERPBaseService;
+import com.wdqsoft.system.common.lang.Result;
+
+public class ErpPurchaseService extends ERPBaseService {
+    @Override
+    protected Result selectByAllList(JSONObject object) {
+        return super.selectByAllList(object);
+    }
+}
